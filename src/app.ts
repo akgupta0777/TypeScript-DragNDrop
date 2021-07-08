db89c08aa4531190c235f3b6e6e563400dbe5641
akgupta0777 <akgupta0777@gmail.com>
2021-07-08 19:43:04 +0530

    Added Better Customizable Validation Checks

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,39 @@
+// Validation
+interface Validatable {
+    propertyName: string;
+    value: string | number;
+    required: boolean;
+    minLength?: number;
+    maxLength?: number;
+    min?: number;
+    max?: number;
+}
+
+function validationOf(validatableInput : Validatable){
+    let isValid = true;
+    if(validatableInput.required){
+        isValid = isValid && (validatableInput.value.toString().trim().length !== 0);
+        if(!isValid) console.log("Reason : Value is required!");
+    }
+    if(validatableInput.minLength && typeof validatableInput.value === 'string'){
+        isValid = isValid && validatableInput.value.length >= validatableInput.minLength;
+        if(!isValid) console.log("Reason : minimum length is not maintained");
+    }
+    if(validatableInput.maxLength && typeof validatableInput.value === 'string'){
+        isValid = isValid && validatableInput.value.length <= validatableInput.maxLength;
+        if(!isValid) console.log("Reason : maximum length is not maintained");
+    }
+    if(validatableInput.min){
+        isValid = isValid && (+validatableInput.value >= validatableInput.min);
+        if(!isValid) console.log("Reason : minimum value is not maintained ");
+    }
+    if(validatableInput.max){
+        isValid = isValid && (+validatableInput.value <= validatableInput.max);
+        if(!isValid) console.log("Reason : maximum value is not maintained");
+    }
+    if(!isValid) console.log("Error in : ",validatableInput.propertyName);
+    return isValid;
+}
 
 // AutoBind Decorator that is used for binding 'this' to local bounded functions.
 function autoBind(
@@ -5,7 +41,6 @@ function autoBind(
     methodName:String,
     descriptor:PropertyDescriptor
 ){
-    console.log("Calling Decorator");
     const originalMethod = descriptor.value;
     const adjustableDescriptor: PropertyDescriptor = {
         configurable : true,
@@ -17,6 +52,7 @@ function autoBind(
     return adjustableDescriptor;
 }
 
+// Project Class
 class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
@@ -38,7 +74,7 @@ class ProjectInput {
         this.attach();
         this.configure();   
     }
-    
+
     private attach() {
         this.hostElement.insertAdjacentElement("afterbegin",this.element);
     }
@@ -47,14 +83,20 @@ class ProjectInput {
         const title = this.titleInputElement.value;
         const description = this.descriptionInputElement.value;
         const peopleCount = this.peopleInputElement.value;
-        if(title.trim().length === 0 ||
-        description.trim().length === 0||
-        peopleCount.trim().length === 0){
-            alert("Invalid inputs !, Please try again");
-            return ;
+
+        const titleValidator : Validatable = {propertyName:"Title",value:title,required:true,minLength:5}; 
+        const descriptionValidator : Validatable = {propertyName:"Description",value:description,required:true,minLength:10,maxLength:50}; 
+        const peopleValidator : Validatable = {propertyName:"People",value: +peopleCount,required:true,min:1,max:10}; 
+        
+        if(validationOf(titleValidator) &&
+           validationOf(descriptionValidator) &&
+           validationOf(peopleValidator)
+        ){
+            return [title,description,+peopleCount];
         }
         else{
-            return [title,description,+peopleCount];
+            alert("Invalid inputs ! Please try again");
+            return ;
         }
     }
 
@@ -63,11 +105,10 @@ class ProjectInput {
         this.descriptionInputElement.value = "";
         this.peopleInputElement.value = "";
     }
+
     @autoBind
     private submitHandler(event: Event){
-        console.log("Calling submit handler");
         event.preventDefault();
-        console.log("Submitting data ...");
         const userInputs = this.gatherInputs();
         if(Array.isArray(userInputs)){
             const [title,description,peopleCount] = userInputs;
